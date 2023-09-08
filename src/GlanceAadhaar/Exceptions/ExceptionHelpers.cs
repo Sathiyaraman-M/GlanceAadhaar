78d6a789e9c920140aa37edf8c3de5be8f4c001e
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-09-08 22:10:17 +0530

    Add ApiException and ExceptionHelpers

---
+++ src/GlanceAadhaar/Exceptions/ExceptionHelpers.cs
@@ -0,0 +1,20 @@
+﻿namespace GlanceAadhaar.Exceptions;
+
+internal static class ExceptionHelpers
+{
+    public static T ThrowIfNull<T>(T argument, string argName) where T : class
+    {
+        if (argument == null)
+            throw new ArgumentNullException(argName);
+
+        return argument;
+    }
+    
+    public static string ThrowIfNullOrEmptyString(string argument, string argumentName)
+    {
+        if (string.IsNullOrWhiteSpace(argument))
+            throw new ArgumentException(RequiredNonEmptyString, argumentName);
+
+        return argument;
+    }
+}
