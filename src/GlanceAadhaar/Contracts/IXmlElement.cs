4d52fc92390a0d13dba792a7d6c2efb3d979e137
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-09-08 22:11:34 +0530

    New Interface: IXmlElement

---
+++ src/GlanceAadhaar/Contracts/IXmlElement.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml.Linq;
+
+namespace GlanceAadhaar.Contracts;
+
+internal interface IXmlElement
+{
+    XElement ConvertToXml(string elementName);
+    
+    void LoadFromXml(XElement element);
+}
