32edd0e8952e0d514c9718fd24dbbb6dd81cf1b6
SathiyaramanM <Sathiyaraman2003@gmail.com>
2023-09-27 14:54:06 +0530

    Create root element for PidBlock: PidData

---
+++ src/Elements/PidBlock/PidData.cs
@@ -0,0 +1,16 @@
+﻿namespace GlanceAadhaar.Elements.PidBlock;
+
+public class PidData : IXmlElement
+{
+    public ICollection<Biometric> Biometrics { get; set; } = new List<Biometric>();
+    
+    public XElement ConvertToXml(string elementName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void LoadFromXml(XElement element)
+    {
+        throw new NotSupportedException();
+    }
+}
