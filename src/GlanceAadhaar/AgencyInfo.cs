f496de36b4d943414dc1221502bf95bcb6d6ffe5
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-09-27 06:05:31 +0530

    Add AgencyInfo

---
+++ src/GlanceAadhaar/AgencyInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace GlanceAadhaar;
+
+public class AgencyInfo
+{
+    public string AuaLicenseKey { get; set; }
+    
+    public string AsaLicenseKey { get; set; }
+    
+    public string AuaCode { get; set; }
+    
+    public string SubAuaCode { get; set; }
+    
+    public IDictionary<string, Uri> Hosts { get; set; }
+}
