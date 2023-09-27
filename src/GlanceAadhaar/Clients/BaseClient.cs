2c0e19310ac689c88718561155689d246623f970
SathiyaramanM <sathiyaraman2003@gmail.com>
2023-09-27 06:08:27 +0530

    Add abstract BaseClient

---
+++ src/GlanceAadhaar/Clients/BaseClient.cs
@@ -0,0 +1,13 @@
+﻿using GlanceAadhaar.Requests;
+using GlanceAadhaar.Responses;
+
+namespace GlanceAadhaar.Clients;
+
+public abstract class BaseClient<TRequest, TResponse> where TRequest : BaseRequest where TResponse : BaseResponse
+{
+    public Uri Endpoint { get; set; }
+    
+    protected abstract string XmlElementName { get; }
+
+    public abstract Task<TResponse> ExecuteAsync(TRequest request, CancellationToken cancellationToken = default);
+}
