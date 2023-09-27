4821500f6b650b9eda2ffd5499d9ea9bc5ea8efa
SathiyaramanM <Sathiyaraman2003@gmail.com>
2023-09-27 11:17:49 +0530

    New element: EncryptedPidData

---
+++ src/Elements/EncryptedPidData.cs
@@ -0,0 +1,30 @@
+﻿namespace GlanceAadhaar.Elements;
+
+public class EncryptedPidData : IXmlElement
+{
+    public EncryptedPidData()
+    {
+        
+    }
+
+    public EncryptedPidData(XElement element)
+    {
+        LoadFromXml(element);
+    }
+    
+    public string Data { get; set; }
+    
+    public XElement ConvertToXml(string elementName)
+    {
+        ThrowIfNullOrEmptyString(Data, nameof(Data));
+        
+        return new XElement(elementName, new XAttribute("type",'X'), Data);
+    }
+
+    public void LoadFromXml(XElement element)
+    {
+        ThrowIfNull(element, nameof(element));
+        ThrowIfNullOrEmptyString(element.Value, nameof(element.Value));
+        Data = element.Value;
+    }
+}
